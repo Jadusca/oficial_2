8ff8a189bbaa922be5dde8d387cfa0fb307ce1bd
KuLuis2000 <lku91087@gmail.com>
2025-04-30 22:43:48 -0600

    hacer ver resumen

--- revisar_ficha_licenciatura.php
+++ revisar_ficha_licenciatura.php
@@ -8,72 +8,73 @@
     <script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
     <script src="responsiveslides.min.js"></script>
     <style>
-    .modal {
-        display: none;
-        position: fixed;
-        z-index: 1000;
-        padding-top: 100px;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-        background-color: rgba(0,0,0,0.5);
-    }
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1000;
+            padding-top: 100px;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.5);
+        }
 
-    .modal-content {
-        background-color: #fefefe;
-        margin: auto;
-        padding: 20px;
-        border-radius: 8px;
-        width: 60%;
-    }
+        .modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            padding: 20px;
+            border-radius: 8px;
+            width: 60%;
+        }
 
-    .close {
-        color: #aaa;
-        float: right;
-        font-size: 24px;
-        font-weight: bold;
-        cursor: pointer;
-    }
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 24px;
+            font-weight: bold;
+            cursor: pointer;
+        }
     </style>
 </head>
 <!-- Modal -->
 <div id="resumenModal" class="modal">
-<div class="modal-content">
-    <span class="close" onclick="cerrarModal()">&times;</span>
-    <h3>Resumen</h3>
-    <p id="contenidoResumen"></p>
-</div>
+    <div class="modal-content">
+        <span class="close" onclick="cerrarModal()">&times;</span>
+        <h3>Resumen</h3>
+        <p id="contenidoResumen"></p>
+    </div>
 </div>
 
 <script>
-function mostrarResumen(texto) {
-    document.getElementById("contenidoResumen").innerText = texto;
-    document.getElementById("resumenModal").style.display = "block";
-}
-
-function cerrarModal() {
-    document.getElementById("resumenModal").style.display = "none";
-}
-
-window.onclick = function(event) {
-    var modal = document.getElementById("resumenModal");
-    if (event.target == modal) {
-        cerrarModal();
+    function mostrarResumen(texto) {
+        document.getElementById("contenidoResumen").innerText = texto;
+        document.getElementById("resumenModal").style.display = "block";
     }
-}
+
+    function cerrarModal() {
+        document.getElementById("resumenModal").style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        var modal = document.getElementById("resumenModal");
+        if (event.target == modal) {
+            cerrarModal();
+        }
+    }
 </script>
 
 <?php
 include "headerSuperadmin.php";
 ?>
 
-<div class="menu1">
-    <a class="arrow" href="herramientas.php"><i class="fa-solid fa-arrow-left"></i></a>
+<div class="edit_car">
+    <div class="menu1_1">
+        <a class="arrow" href="modulo_revision_general.php"><i class="fa-solid fa-arrow-left"></i></a>
+    </div>
+    <h2 class="tit_mod_car">Documentos de Licenciatura Pendientes</h2>
 </div>
-
-<br>
 
 <?php
 require 'conexion.php';
@@ -88,7 +89,6 @@ $query = "SELECT f.*, c.nombre_carrera, t.nombre_titulacion, e.nombre_estado
 $resultado = $conectar->query($query);
 ?>
 
-<h2 class="tit_mod_car">Documentos de Licenciatura Pendientes</h2>
 <section class="tabla-contenedor">
     <table class="tab_mod_1">
         <tr>
@@ -114,7 +114,12 @@ $resultado = $conectar->query($query);
                 <td><?php echo $row['nombre_titulacion']; ?></td>
                 <td><?php echo $row['asesor_interno']; ?></td>
                 <td><?php echo $row['asesor_externo']; ?></td>
-                <td><button onclick="mostrarResumen(`<?php echo htmlspecialchars($row['resumen'], ENT_QUOTES); ?>`)">Ver resumen</button></td>
+                <td>
+                    <div class="pdf_busqueda">
+                        <button onclick="mostrarResumen(`<?php echo htmlspecialchars($row['resumen'], ENT_QUOTES); ?>`)"><i
+                                class="fa-solid fa-file-contract"></i></button>
+                    </div>
+                </td>
                 <td><?php echo $row['fecha']; ?></td>
                 <td><?php echo $row['palabras_clave']; ?></td>
                 <td><?php echo $row['paginas']; ?></td>
@@ -124,18 +129,22 @@ $resultado = $conectar->query($query);
                     <div class="actions_1">
                         <div class="icon_action">
                             <div class="pdf_busqueda">
-                                <a href="ver_documento.php?archivo=<?php echo $row['documento']; ?>" target="_blank"><i class="fa-solid fa-file-pdf"></i></a>
+                                <a href="ver_documento.php?archivo=<?php echo $row['documento']; ?>" target="_blank"><i
+                                        class="fa-solid fa-file-pdf"></i></a>
                             </div>
                             <div class="pdf_busqueda">
-                                <a href="ver_oficio.php?archivo=<?php echo $row['oficio']; ?>" target="_blank"><i class="fa-solid fa-file-zipper"></i></a>
+                                <a href="ver_oficio.php?archivo=<?php echo $row['oficio']; ?>" target="_blank"><i
+                                        class="fa-solid fa-file-zipper"></i></a>
                             </div>
                         </div>
                         <div class="icon_action">
-                            <div class="pdf_busqueda">
-                                <a href="aprobar.php?id=<?php echo $row['id_ficha_carrera']; ?>&tipo=lic"><i class="fa-solid fa-square-check"></i></a>
+                            <div class="pdf_busqueda_check">
+                                <a href="aprobar.php?id=<?php echo $row['id_ficha_carrera']; ?>&tipo=lic"><i
+                                        class="fa-solid fa-square-check"></i></a>
                             </div>
-                            <div class="pdf_busqueda">
-                                <a href="rechazar.php?id=<?php echo $row['id_ficha_carrera']; ?>&tipo=lic"><i class="fa-solid fa-rectangle-xmark"></i></a>
+                            <div class="pdf_busqueda_trash">
+                                <a href="rechazar.php?id=<?php echo $row['id_ficha_carrera']; ?>&tipo=lic"><i
+                                        class="fa-solid fa-rectangle-xmark"></i></a>
                             </div>
                         </div>
                     </div>
@@ -144,3 +153,9 @@ $resultado = $conectar->query($query);
         <?php endwhile; ?>
     </table>
 </section>
+
+<br><br>
+
+<?php
+include "footer.php";
+?>
